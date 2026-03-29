7f659634689cb1d0bffcefdee79aad24001a5e73
MaykonRO <maykonro2004@gmail.com>
2026-03-28 22:16:00 -0300

    feat: adicionando as funções de ler e escrever e somente escrita de texto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,43 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main {
+    public static String writeIO(String msg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(msg);
+        return scanner.nextLine();
+
+    }
+
+    public static void write(String msg){
+        System.out.println(msg);
+    }
+
+    public static String inverteString(String texto){
+        Queue<String> fila = new LinkedList<>();
+
+        for (char letra: texto.toCharArray()){
+            pilha.push(letra);
+        }
+        StringBuilder saida = new StringBuilder();
+        while(!pilha.isEmpty()){
+            saida.append(pilha.pop().toString());
+        }
+
+        return saida.toString();
+    }
+
     static void main() {
+
+        String teste = writeIO("oi");
+        write("oi");
+
 
 
     }
+
+
 }
