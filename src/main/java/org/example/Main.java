23fb51fba78b8d47805ab37a4bc9254d1c769847
MaykonRO <maykonro2004@gmail.com>
2026-03-28 23:08:20 -0300

    feat: adição da função 1 e 3, falta a função 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,25 +16,48 @@ public class Main {
         System.out.println(msg);
     }
 
-    public static String inverteString(String texto){
-        Queue<String> fila = new LinkedList<>();
-
-        for (char letra: texto.toCharArray()){
-            pilha.push(letra);
-        }
-        StringBuilder saida = new StringBuilder();
-        while(!pilha.isEmpty()){
-            saida.append(pilha.pop().toString());
-        }
-
-        return saida.toString();
-    }
 
     static void main() {
+        Queue<String> fila = new LinkedList<>();
+        Integer op;
+        do {
+            System.out.println("### Gestor de paginas web");
+            System.out.println("1) Retirar senha comum.");
+            System.out.println("2) Retirar senha preferencial.");
+            System.out.println("3) Chamar próximo a ser atendido.");
+            op = Integer.parseInt(writeIO("4) - SAir"));
 
-        String teste = writeIO("oi");
-        write("oi");
+            switch (op) {
+                case 1:
 
+                    if (!fila.isEmpty()){
+                        Integer ultimoInt;
+                        String pegaUltimo = ((LinkedList<String>) fila).getLast();
+                        ultimoInt = Integer.parseInt(pegaUltimo);
+                        ultimoInt += 1;
+                        fila.add(ultimoInt.toString());
+                        write("a sua senha é: "+ultimoInt.toString());
+                    }
+                    else {
+                        fila.add("1");
+                    }
+                    write(fila.toString());
+//                    writeIO("sua senha é: ");
+                    break;
+                case 2:
+                    //System.out.println("Ultima pagina adicionada: ");
+                    //System.out.println(historico.isEmpty() ? "Historico vazio" : historico.peek());
+                    write("abcv");
+                    break;
+                case 3:
+                    write("o numero chamado é: " + fila.poll());
+                    write(fila.toString());
+                    break;
+            }
+
+
+        }
+        while (op != 4);
 
 
     }
