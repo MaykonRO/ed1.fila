4860c5ad7068cabf8cca0c1b243c087655c86ae7
LAB32 <lab32@fatec.com>
2026-03-31 20:05:42 -0300

    terminou

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,50 +12,63 @@ public class Main {
 
     }
 
-    public static void write(String msg){
+    public static void write(String msg) {
         System.out.println(msg);
     }
 
 
-    static void main() {
+    public static void main(String[] args) {
         Queue<String> fila = new LinkedList<>();
-        Integer op;
+        Queue<String> filaP = new LinkedList<>();
+        int op, aux = 2;
         do {
             System.out.println("### Gestor de paginas web");
             System.out.println("1) Retirar senha comum.");
             System.out.println("2) Retirar senha preferencial.");
             System.out.println("3) Chamar próximo a ser atendido.");
-            op = Integer.parseInt(writeIO("4) - SAir"));
+            op = Integer.valueOf(writeIO("4) - SAir"));
 
             switch (op) {
                 case 1:
 
-                    if (!fila.isEmpty()){
+                    if (!fila.isEmpty()) {
                         Integer ultimoInt;
                         String pegaUltimo = ((LinkedList<String>) fila).getLast();
-                        ultimoInt = Integer.parseInt(pegaUltimo);
+                        ultimoInt = Integer.valueOf(Integer.parseInt(pegaUltimo));
                         ultimoInt += 1;
                         fila.add(ultimoInt.toString());
-                        write("a sua senha é: "+ultimoInt.toString());
-                    }
-                    else {
+                        write("a sua senha é: " + ultimoInt.toString());
+                    } else {
                         fila.add("1");
                     }
                     write(fila.toString());
-//                    writeIO("sua senha é: ");
                     break;
                 case 2:
-                    //System.out.println("Ultima pagina adicionada: ");
-                    //System.out.println(historico.isEmpty() ? "Historico vazio" : historico.peek());
-                    write("abcv");
+                    if (!filaP.isEmpty()) {
+                        Integer ultimoInt;
+                        String pegaUltimo = ((LinkedList<String>) filaP).getLast();
+                        ultimoInt = Integer.valueOf(Integer.parseInt(pegaUltimo));
+                        ultimoInt += 1;
+                        filaP.add(ultimoInt.toString());
+                        write("a sua senha é: " + ultimoInt.toString());
+                    } else {
+                        filaP.add("1");
+                    }
+                    write(filaP.toString());
                     break;
                 case 3:
-                    write("o numero chamado é: " + fila.poll());
-                    write(fila.toString());
+                    if (!fila.isEmpty()) {
+                        if (aux > 0) {
+                            write("o senha comum é: " + fila.poll());
+                            write(fila.toString());
+                            aux--;
+                        } else {
+                            write("o senha prefencial é: " + filaP.poll());
+                            aux = 2;
+                        }
+                    }
                     break;
             }
-
-
         }
         while (op != 4);
 
